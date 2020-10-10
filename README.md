4afd1e65e7463bd183ce3aca49f8c913c4f01a73
zaze <359635919@qq.com>
2020-10-10 18:44:22 +0800

    1

---
+++ README.md
@@ -0,0 +1,1 @@
+[1. Kotlin协程笔记](https://share.mubu.com/doc/2XfUPv7fnVG)
---
+++ src/com/zaze/kotlin/example/MyLog.kt
@@ -0,0 +1,7 @@
+package com.zaze.kotlin.example
+
+object MyLog {
+    fun i(tag: String, message: String) {
+        println("$tag: $message")
+    }
+}
