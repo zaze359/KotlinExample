156539a71ab281ec200b5625b6d8e9185b315c7b
zaze <359635919@qq.com>
2020-10-20 14:07:53 +0800

    1

--- src/com/zaze/kotlin/example/coroutine/Main.kt
+++ src/com/zaze/kotlin/example/coroutine/Main.kt
@@ -1,7 +1,37 @@
 package com.zaze.kotlin.example.coroutine
 
+import com.zaze.kotlin.example.DateUtil
 import com.zaze.kotlin.example.MyLog
+import java.lang.Exception
+import javax.xml.crypto.Data
+import kotlin.concurrent.thread
+import kotlin.coroutines.Continuation
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
 
-fun main() {
-    MyLog.i("aa", "bb")
+suspend fun main() {
+    MyLog.i(DateUtil.timeMillisToString(), "main start")
+    val result = testSuspendable("testSuspendable")
+    MyLog.i(DateUtil.timeMillisToString(), result)
+    MyLog.i(DateUtil.timeMillisToString(), "main finish")
+}
+
+suspend fun testSuspendable(url: String): String {
+    return suspendCoroutine<String> { continuation ->
+        thread {
+            try {
+                // 将正常的结果返回
+                continuation.resume(download(url))
+            } catch (e: Exception) {
+                // 将异常返回
+                continuation.resumeWithException(e)
+            }
+        }
+    }
+}
+
+fun download(url: String): String {
+    Thread.sleep(2000L)
+    return url
 }
